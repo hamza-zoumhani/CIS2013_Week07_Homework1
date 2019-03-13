834b48b6a7a798b9a8ff4a2482999741a791928d
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-03-13 15:54:37 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+
+using namespace std;
+
+
+void getName(string name){
+	cout << "Enter the name of the recipient: ";
+	cin >> name;
+}
+
+int main(){
+	ifstream infile;
+	ofstream outfile;
+	
+	cout << "Welcome to junk mail generator!" << endl;
+	cout << endl;
+	
+	
+	
+	getName(n);
+	
+	
+	return 0;
+}
